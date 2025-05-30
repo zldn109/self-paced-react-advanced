61b1b956ad60f505dc5db4ee675e4a6f50c50908
zldn109 <ss771009@naver.com>
2025-05-30 23:00:38 +0900

    refactor: 불필요한 주석 제거

--- src/contexts/AppState.jsx
+++ src/contexts/AppState.jsx
@@ -14,29 +14,3 @@ export const restaurantsState = atom({
   key: "restaurantsState",
   default: [],
 });
-
-// export function AppProvider({ children }) {
-//   const MODAL_TYPES = {
-//     ADD: "add",
-//     DETAIL: "detail",
-//   };
-//   Object.freeze(MODAL_TYPES);
-
-//
-//
-
-//   return (
-//     <AppContext.Provider
-//       value={{
-//         modalTypeToOpen,
-//         setModalTypeToOpen,
-//         clickedRestaurantInfo,
-//         setClickedRestaurantInfo,
-//         restaurants,
-//         setRestaurants,
-//       }}
-//     >
-//       {children}
-//     </AppContext.Provider>
-//   );
-// }
